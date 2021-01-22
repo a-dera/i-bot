587268e5150b6b8713faf046c64da8aea5b0485d
Amédée DERA <dera.amedee@gmail.com>
2021-01-22 10:12:48 +0000

    réorganisation des tables

--- build/scripts/words_table.js
+++ build/scripts/words_table.js
@@ -1,12 +1,13 @@
 var date_test = [
-    "date",
-    "test",
-    "date du test",
-    "date des test"
-];
-
-var lieu_test = [
-    "lieu",
-    "lieu du test",
-    "où se passe le test",
+  "date",
+  "test",
+  "date du test",
+  "date des test",
+  "lieu",
+  "lieu du test",
+  "où se passe le test",
+  "matières du test",
+  "matière test",
+  "dossier",
+  "dossier de candidature du test",
 ];
